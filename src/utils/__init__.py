7b1cf03381817517f8a20c0ef79ec65538dbd3be
Philip Korsager Nickel <philipnickel@Philips-MacBook-Pro.local>
2025-11-28 08:42:27 +0100

    updats

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -3,8 +3,9 @@
 Import conveniences:
 - from utils import plotting    # For plotting operations
 - from utils import cli         # For command-line argument parsing
+- from utils import mlflow_io   # For MLflow I/O operations
 """
 
-from . import plotting, cli
+from . import plotting, cli, mlflow_io
 
-__all__ = ["plotting", "cli"]
+__all__ = ["plotting", "cli", "mlflow_io"]
