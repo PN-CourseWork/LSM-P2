32c1957ec1abbcee2c879bfd969e2eef1eee7165
Philip Korsager Nickel <philipnickel@outlook.dk>
2025-11-30 17:46:11 +0100

    fixup

--- main.py
+++ main.py
@@ -2,13 +2,12 @@
 """Main entry point for project management."""
 
 import sys
-from src.utils.TUI import batch
-from src.utils.TUI.tui import run_tui
+from src.utils.tui import handle_args, run_tui
 
 def main():
     """Run interactive TUI if no args, else run batch mode."""
     if len(sys.argv) > 1:
-        batch.handle_args()
+        handle_args()
     else:
         run_tui()
 
